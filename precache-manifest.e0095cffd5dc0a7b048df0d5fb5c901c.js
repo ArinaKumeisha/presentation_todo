cc34dd0cd3fa62f36475f376e3e3d09035c8c450
ArinaKumeisha <arinanozdracheva@mail.ru>
2021-11-18 16:26:42 +0300

    Updates

---
+++ precache-manifest.e0095cffd5dc0a7b048df0d5fb5c901c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcfb9074349469bcf666408131e78bf",
+    "url": "/presentation_todo/index.html"
+  },
+  {
+    "revision": "3081a17f520a0ed91449",
+    "url": "/presentation_todo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "e21a2b6884be81eca37b",
+    "url": "/presentation_todo/static/js/2.3670533f.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/presentation_todo/static/js/2.3670533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3081a17f520a0ed91449",
+    "url": "/presentation_todo/static/js/main.547f4690.chunk.js"
+  },
+  {
+    "revision": "b6a0c38f3516144622d7",
+    "url": "/presentation_todo/static/js/runtime-main.d39ca8a6.js"
+  }
+]);
